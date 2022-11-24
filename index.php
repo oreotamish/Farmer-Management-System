db29ba21176f31e280d57d66959ce0a60f9f6d57
sudoVenomSnake <swastikm26@gmail.com>
2022-11-24 20:17:27 +0530

    First Commit

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Farmer Management System</title>
+    <!-- Font Awesome -->
+    <link
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+    rel="stylesheet"
+    />
+    <!-- Google Fonts -->
+    <link
+    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+    rel="stylesheet"
+    />
+    <!-- MDB -->
+    <link
+    href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.0.1/mdb.min.css"
+    rel="stylesheet"
+    />
+</head>
+<body>
+
+    <nav class="navbar navbar-expand-lg fixed-top navbar-scroll shadow-0" style="background-color: #5ec726;">
+    <div class="container">
+        <a class="navbar-brand" href="#">Farmer Management System</a>
+        <button class="navbar-toggler ps-0" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarExample01"
+        aria-controls="navbarExample01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="d-flex justify-content-start align-items-center">
+            <i class="fas fa-bars"></i>
+        </span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarExample01">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item active">
+            <a class="nav-link px-3" href="#!">Crop Health</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link px-3" href="#!">Cattle</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link px-3" href="#!">License</a>
+            </li>
+            <li class="nav-item active">
+            <a class="nav-link px-3" href="#!">Seeds</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link px-3" href="#!">Equiptment</a>
+            </li>
+        </ul>
+
+        <ul class="navbar-nav flex-row">
+            <li class="nav-item">
+            <a class="nav-link pe-3" href="#!">
+                <i class="fab fa-youtube"></i>
+            </a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link px-3" href="#!">
+                <i class="fab fa-facebook-f"></i>
+            </a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link ps-3" href="#!">
+                <i class="fab fa-instagram"></i>
+            </a>
+            </li>
+        </ul>
+        </div>
+    </div>
+    </nav>
+
+    <!-- MDB -->
+    <script
+    type="text/javascript"
+    src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.0.1/mdb.min.js"
+    ></script>
+</body>
+</html>
